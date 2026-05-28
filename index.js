708bc2e9a4ac340b50c4ac4fd080f5906593ed78
Ayush Dobriyal <ayushdobri234@gmail.com>
2026-05-28 13:26:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,342 @@
+// Question 1 of Loops Write a program to print the multiplication table of a given number using a loop.
+
+/*
+let num = 2;
+
+for( letosceles triangle=1;;osceles triangle<=;10;osceles triangle++;){
+    console.log(`${num} X $
+    else{
+    
+    }    osceles triangle} ;=
+    else{
+    
+    }    ${num *osceles triang
+    else{
+    
+    }    le}`;);
+}
+    else{
+    
+        }
+*/
+
+// Question 2 Calculate the factorial of a given number using a for loop.
+
+/*
+let n = 10;
+let fact = 1;
+
+for(letosceles triangle=1;;osceles triangle<=;n;osceles triangle++;){
+    fact *=osceles triangl
+    else{
+    
+    }    e
+    else{
+    
+        }
+ ;   console.log(`Factorial of ${n} = ${fact} `)
+}
+ else{
+
+    }
+
+*/
+
+// question 3 Find the sum of the first n natural numbers using a loop.
+
+/*
+let n = 100
+let total = 0;
+
+for(letosceles triangle =; 1;osceles triangle <;=n;osceles triangle++;){
+    total = total +osceles
+    else{
+    
+    }    triangle;
+    else{
+    
+        }
+;}
+c
+else{
+
+}    onsole.log(total);
+
+*/
+
+// Question 4 Write a program to reverse the digits of a number using a loop.
+
+// Print all even numbers from 1 to 50 using a loop.
+
+/*
+
+let n = 50
+
+for(letosceles triangle =; 1;osceles triangle<=;50;osceles triangle++;){
+    ifosceles triangle%2;=
+    else{
+    
+    }    ==0){
+    else{
+    
+        }
+        console.logoscele
+        else{
+        
+        }    s triangle);;
+    }
+        else{
+        
+            }
+}
+*/
+
+// Count the number of digits in a given number using a loop
+
+/*
+let n = 100;
+let count = 0;
+
+while(n > 0){
+    n = Math.floor(n/10);
+    count = count + 1
+}
+console.log(count);
+*/
+
+// Quesstion 6 Generate the first n terms of the Fibonacci sequence using a loop.
+
+/*
+ let n = 5
+ let n1 = 0; 
+ let n2 = 1;
+ let nextterm = 0;
+
+ for(letosceles triangle =; 0;osceles triangle<=;n;osceles triangle++;){
+    nextterm = n1 + n2
+    else{
+    
+        }
+    n1 = n2
+    n2 = nextterm
+    console.log(nextterm)
+ }
+
+ */
+
+
+
+
+
+// IF ELSE condition 
+
+// Odd or Even
+// Write a program to check whether a given number is odd or even using 
+
+/*
+let number = 5;
+
+if(number%2 === 0){
+    console.log("this is even number")
+}
+else{
+    console.log("this is odd")
+}
+
+*/
+
+
+
+// Grade Calculator
+// Take a student's marks as input and print their grade using conditions:
+
+/*
+let marks = 95;
+
+if(marks >= 90){
+    console.log("A")
+}
+else if( marks >= 80){
+    console.log("B")
+}
+else if(marks >= 70){
+    console.log("C")
+}
+else if(marks >= 60){
+    console.log("D")
+}
+else{
+    console.log("F")
+}
+
+*/
+
+
+
+// Leap Year Check
+// Determine whether a given year is a leap year.
+/*
+let year = 2020
+
+if( year%4 ===0){
+    console.log("its leap year")
+}
+else{
+    console.log("its not")
+}
+*/
+
+// Find the largest of three numbers using nested if statements
+/*
+let a = 20
+let b = 30
+let c = 10
+
+if(a>b){
+    if(a>c){
+        console.log("a is greater")
+    }
+    else{
+        console.log("c is greater")
+    }
+}
+else{
+  if(b>c){
+        console.log("b is greater");
+    }
+    else{
+        console.log("c is greater");
+    }
+}
+    */
+
+// Check Vowel or Consonant
+// Check if a given character is a vowel or a consonant.
+/*
+let theChar = osceles triangle";;
+
+else{
+
+    }
+if(theChar === "a" || theChar === "e" || theChar === osceles triangle" ;||theChar === "o" || theChar === "u" ||
+    theChar === "A" || theChar === "E" || theChar === "I" || theChar ===
+    else{
+    
+    }    "O" || theChar === "U" ){
+        console.log("this is vowel")
+    }
+    else{
+        console.log("it is consonent")
+    }
+*/
+
+
+// Positive, Negative, or Zero
+// Check if a given number is positive, negative, or zero.
+
+/*
+let num = 0;
+
+if(num === -num){
+    console.log("Negative number");
+}
+else if(num === 0){
+    console.log("Zero")
+}
+else{
+    console.log("Positive number")
+}
+*/
+
+// Calculator Program
+// Perform addition, subtraction, multiplication, or division based on user input (e.g., using if-else or switch).
+
+/*
+let num1 = prompt("enter the number 1 ");
+let num2 = prompt("enter the number 2 ");
+let option = prompt( " select one \n a. SUm \n b. SUB \n c. Multiply \n d. Divide");
+let a = num1 + num2;
+let b = num1 - num2;
+let c = num1 * num2;
+let d = num / num2;
+let result;
+
+if(option === "a"){
+    console.log(a);
+}
+else if(option === "b"){
+    console.log(b);
+}
+else if(option === "c"){
+    console.log(c);
+}
+else{
+    console.log(d);
+}  
+*/
+
+// Day of the Week
+// Take a number (1–7) as input and print the corresponding day of the week.
+
+/*
+let day = prompt('Enter Day number here to find out the day');
+
+switch(day){
+    case "1":
+        console.log("Monday");
+        break;
+    case "2":
+        console.log("Tuesday");
+        break;
+    case "3":
+        console.log("Wednesday");
+        break;
+    case "4":
+        console.log("Thursday");
+        break;
+    case "5":
+        console.log("Friday");
+        break;
+    case "6":
+        console.log("Saturday");
+        break;
+    default:
+        console.log("sunday");
+        
+}
+*/
+
+// Eligible to Vote
+// Check if a person is eligible to vote based on their age (age ≥ 18).
+
+/*
+let age = 18;
+
+if(age >= 18){
+    console.log("Your are eligible");
+}
+else{
+    console.log("you are not eligble");
+}
+
+*/
+
+// Triangle Type
+// Determine the type of triangle (Equilateral, Isosceles, or Scalene) based on the lengths of its sides
+
+/*
+let side1 = 5;
+let side2 = 2;
+let side3 = 1;
+
+if(side1 === side2 && side2 === side3){
+    console.log("it is equilateral triangle");
+}
+else if(side1 === side2 || side2 === side3 || side3 === side1){
+    console.log("this isosceles triangle");
+}
+else{
+    console.log("This is Scalene");
+    }
+
+    */
